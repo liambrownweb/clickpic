e62ed96d3c015826ef3c68d8393f6d3e64831ea3
Liam <lima@linux-vd2f.site>
2013-12-11 08:52:17 -0600

    Node HTTP server at partial functionality.

--- node/routes/images.js
+++ node/routes/images.js
@@ -2,6 +2,9 @@
  * GETs something from the images directory.
  */
 
-exports.main = function(req, res){
-  res.render('images', {title: 'Image'});
+exports.main = function (req, res) {
+    if (req.query.hasOwnProperty("file")) {
+        console.log("attempting to load file...");
+        res.sendfile("/srv/www/htdocs/clickpic/data/images/"+req.query.file);
+    }
 };
--- node/server.js
+++ node/server.js
@@ -1,38 +1,38 @@
-var express = require('express'),
-    app = express(),
-    http = require('http'),
-    httpServer = http.createServer(app),
-    path = require('path'),
-    routes = require('./routes'),
-    user = require('./routes/user');
-    images = require('./routes/images');
+    var express = require('express'),
+        app = express(),
+        http = require('http'),
+        httpServer = http.createServer(app),
+        path = require('path'),
+        routes = require('./routes'),
+        user = require('./routes/user'),
+        images = require('./routes/images');
 
 
-// all environments
-app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jshtml');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('your secret here'));
-    app.use(express.session());
-    app.use(app.router);
-    app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-    app.use(express.static(path.join(__dirname, 'public')));
-});
+    // all environments
+    app.configure(function () {
+        app.set('port', process.env.PORT || 3000);
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('view engine', 'ejs');
+        app.use(express.favicon());
+        app.use(express.logger('dev'));
+        app.use(express.json());
+        app.use(express.urlencoded());
+        app.use(express.methodOverride());
+        app.use(express.cookieParser('your secret here'));
+        app.use(express.session());
+        app.use(app.router);
+        app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+        app.use(express.static(path.join(__dirname, 'public')));
+    });
 
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
-httpServer.listen(app.get('port'), function () {
-    console.log("Express server listening on port %s.", httpServer.address().port);
-});
+    // development only
+    if ('development' === app.get('env')) {
+      app.use(express.errorHandler());
+    }
+    httpServer.listen(app.get('port'), function () {
+        console.log("Express server listening on port %s.", httpServer.address().port);
+    });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/images', images.main);
+    app.get('/', routes.index);
+    app.get('/users', user.list);
+    app.get('/images/get', images.main);
